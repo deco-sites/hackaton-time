85df309624192cc03625ae8203955162ca874dd6
André Luiz Bastos Carneiro <andre548luiz@gmail.com>
2024-07-14 13:47:34 +0000

    Release staging environment with 0 changes made by

--- sections/Colecao.tsx
+++ sections/Colecao.tsx
@@ -7,7 +7,7 @@ export default function (props: props) {
   return (
     <div class="py-12 max-w-[1920px] mx-auto">
       <div class="text-white text-9xl">
-        <p>
+        <p class="font-bold">
           {props.titlecolection}{" "}
           <span class="text-info tracking-widest">{props.yearcolection}</span>
         </p>
